c4ddfb169a38a722a7f4a25f5b11c16c6fd525fe
Paris Hsu <peggiehsu@Peggies-MacBook-Pro.local>
2015-02-12 11:41:01 +0100

    Initial Commit

---
+++ curve/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  curve
+//
+//  Created by Paris Hsu on 2/12/15.
+//  Copyright (c) 2015 Peggie hsu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
